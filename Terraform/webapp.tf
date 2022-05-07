0e375d16bba093074b9b679d59cdcd299a87e265
harish lakshmipuram <harish.lakshmipuram1@outlook.com>
2022-05-07 10:58:01 +0000

    Updated webapp.tf

--- Terraform/webapp.tf
+++ Terraform/webapp.tf
@@ -1,15 +1,16 @@
- terraform {
+terraform {
   required_version = ">= 0.11" 
- backend "azurerm" {
+  backend "azurerm" {
   storage_account_name = "__terraformstorageaccount__"
     container_name       = "terraform"
     key                  = "terraform.tfstate"
-	access_key  ="__storagekey__"
-	}
-	}
-  provider "azurerm" {
-  features {}
+    access_key  ="__storagekey__"
+    }
 }
+provider "azurerm" {
+    features {} 
+  }
+
 resource "azurerm_resource_group" "dev" {
   name     = "PULTerraform"
   location = "West Europe"
